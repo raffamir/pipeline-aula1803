bc36cb51a8e815939464b20b72389df0eee0768a
raffamir <raffamir@gmail.com>
2026-03-23 18:16:38 -0300

    Testando Sonar 5

--- src/soma.js
+++ src/soma.js
@@ -1,5 +1,8 @@
 function soma(a, b) {
- return a + b;
+  if (a === 0) {
+    return b;
+  }
+  return a + b;
 }
 
 module.exports = soma;
--- tests/soma.test.js
+++ tests/soma.test.js
@@ -2,3 +2,5 @@ const soma = require('../src/soma');
 test('soma 2 + 3', () => {
   expect(soma(2, 3)).toBe(5);
 });
+
+x = 5
