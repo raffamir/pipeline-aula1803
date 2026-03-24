842423b11ba3299d4aff08911bfca236d8aeaefb
raffamir <raffamir@gmail.com>
2026-03-24 11:15:45 -0300

    Testando Sonar 12

--- src/soma.js
+++ src/soma.js
@@ -7,13 +7,24 @@ function soma(a, b) {
 }
 */
 
-
+/*
 function soma(a, b) {
 
   eval("console.log('executando')");
 
   return a + b;
 }
+*/
+
+function soma(a, b) {
+
+  let x = 10;
+  let x = 20;
+
+  return a + b;
+
+}
+
 
 
 module.exports = soma;
