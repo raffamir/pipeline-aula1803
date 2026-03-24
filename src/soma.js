3ddce99801e3c44814bad8098406cf035ad5aab3
raffamir <raffamir@gmail.com>
2026-03-24 10:55:49 -0300

    Testando Sonar 10

--- src/soma.js
+++ src/soma.js
@@ -1,8 +1,19 @@
+/*
 function soma(a, b) {
   if (a === 0) {
     return b;
   }
   return a + b;
 }
+*/
+
+
+function soma(a, b) {
+
+  eval("console.log('executando')");
+
+  return a + b;
+}
+
 
 module.exports = soma;
