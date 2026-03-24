e000fffb4138a6d9f61310dabb0f3b90f3f0bd47
raffamir <raffamir@gmail.com>
2026-03-24 12:08:51 -0300

    Testando Sonar 23

--- src/soma.js
+++ src/soma.js
@@ -1,23 +1,8 @@
-/*
 function soma(a, b) {
   if (a === 0) {
     return b;
   }
   return a + b;
 }
-*/
-
-
-
-function login(user, pass) {
-
-  const password = "admin123";
-
-  if(pass === password){
-    return true;
-  }
-
-}
-
 
 module.exports = soma;
